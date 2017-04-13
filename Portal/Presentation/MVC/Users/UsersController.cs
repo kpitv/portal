ea5649b4265b5e8289d0bc4e655e10e14b774615
Stas Volyansky <stas.sphere@hotmail.com>
2017-04-12 17:04:04 -0700

    minor fixes

--- Portal/Presentation/MVC/Users/UsersController.cs
+++ Portal/Presentation/MVC/Users/UsersController.cs
@@ -1,9 +1,9 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Portal.Presentation.Identity.Services;
-using System.Threading.Tasks;
 
-namespace Presentation.MVC.Users
+namespace Portal.Presentation.MVC.Users
 {
     [Authorize]
     public class UsersController : Controller
@@ -31,9 +31,9 @@ namespace Presentation.MVC.Users
         }
 
         [HttpGet]
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
-            manager.SignIn.SignOutAsync();
+            await manager.SignIn.SignOutAsync();
             return RedirectToAction(nameof(Login));
         }
     }
